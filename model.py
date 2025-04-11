13e03bfdfa4e39f2de9736e8c644a684e58f9887
Leon Scharwächter <56418155+lscharwaechter@users.noreply.github.com>
2025-04-11 10:31:49 +0200

    Update model.py

--- model.py
+++ model.py
@@ -1,110 +1,73 @@
-# -*- coding: utf-8 -*-
-
 import torch
-from torch import nn
-import math
-import numpy as np
+import torch.nn as nn
 
-class Block(nn.Module):
-    def __init__(self, in_ch, out_ch, time_emb_dim, up=False):
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, time_emb_dim, text_emb_dim):
         super().__init__()
-        self.time_mlp =  nn.Linear(time_emb_dim, out_ch)
-        if up:
-            self.conv1 = nn.Conv2d(2*in_ch, out_ch, 3, padding=1)
-            self.transform = nn.ConvTranspose2d(out_ch, out_ch, 4, 2, 1)
-        else:
-            self.conv1 = nn.Conv2d(in_ch, out_ch, 3, padding=1)
-            self.transform = nn.Conv2d(out_ch, out_ch, 4, 2, 1)
-        self.conv2 = nn.Conv2d(out_ch, out_ch, 3, padding=1)
-        self.bnorm1 = nn.BatchNorm2d(out_ch)
-        self.bnorm2 = nn.BatchNorm2d(out_ch)
-        self.relu  = nn.ReLU()
-        
-    def forward(self, x, t, ):
-        # First Conv
-        h = self.bnorm1(self.relu(self.conv1(x)))
-        # Time embedding
-        time_emb = self.relu(self.time_mlp(t))
-        # Extend last 2 dimensions
-        time_emb = time_emb[(...),np.newaxis,np.newaxis]
-        # Add time channel
-        h = h + time_emb
-        # Second Conv
-        h = self.bnorm2(self.relu(self.conv2(h)))
-        # Down or Upsample
-        return self.transform(h)
+        self.time_mlp = nn.Linear(time_emb_dim, out_channels)
+        self.text_mlp = nn.Linear(text_emb_dim, out_channels)
+        self.block1 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, padding=1),
+            nn.GroupNorm(8, out_channels),
+            nn.ReLU()
+        )
+        self.block2 = nn.Sequential(
+            nn.Conv2d(out_channels, out_channels, 3, padding=1),
+            nn.GroupNorm(8, out_channels),
+            nn.ReLU()
+        )
+        self.residual = nn.Conv2d(in_channels, out_channels, 1)
 
+    def forward(self, x, t, text):
+        h = self.block1(x)
+        t_emb = self.time_mlp(t).view(t.size(0), -1, 1, 1)
+        text_emb = self.text_mlp(text).view(text.size(0), -1, 1, 1)
+        h = h + t_emb + text_emb
+        h = self.block2(h)
+        return h + self.residual(x)
 
-class PositionalEncoding(nn.Module):
-    def __init__(self, dim):
+class UNet(nn.Module):
+    def __init__(self, in_channels=3, out_channels=3, time_emb_dim=256, raw_text_dim=768, text_emb_dim=256):
         super().__init__()
-        self.dim = dim
-
-    def forward(self, time):
-        
-        device = time.device
-        half_dim = self.dim // 2
-        embeddings = math.log(10000) / (half_dim - 1)
-        embeddings = torch.exp(torch.arange(half_dim, device=device) * -embeddings)
-        embeddings = time[:, None] * embeddings[None, :]
-        embeddings = torch.cat((embeddings.sin(), embeddings.cos()), dim=-1)
-
-        return embeddings
-
-
-class SimpleUnet(nn.Module):
-    """
-    A simplified version of the Unet architecture.
-    """
-    def __init__(self):
-        super().__init__()
-        image_channels = 3
-        down_channels = (64, 128, 256, 512, 1024)
-        up_channels = (1024, 512, 256, 128, 64)
-        out_dim = 3 
-        time_emb_dim = 32
-
-        # Time embedding
         self.time_mlp = nn.Sequential(
-                PositionalEncoding(time_emb_dim),
-                nn.Linear(time_emb_dim, time_emb_dim),
-                nn.ReLU()
-            )
-        
-        # Initial projection (from image channels to nr. of kernels)
-        self.conv0 = nn.Conv2d(image_channels, down_channels[0], 3, padding=1)
+            nn.Linear(1, time_emb_dim),
+            nn.ReLU(),
+            nn.Linear(time_emb_dim, time_emb_dim)
+        )
+        self.text_proj = nn.Sequential(
+            nn.Linear(raw_text_dim, text_emb_dim),
+            nn.ReLU(),
+            nn.Linear(text_emb_dim, text_emb_dim)
+        )
 
-        # Downsample
-        self.downstream = nn.ModuleList()
-        for i in range(len(down_channels)-1):
-            self.downstream.append(
-                Block(down_channels[i],down_channels[i+1], time_emb_dim)
-            )
+        # Encoder-Part
+        self.encoder1 = ResidualBlock(in_channels, 64, time_emb_dim, text_emb_dim)
+        self.down1 = nn.Conv2d(64, 128, 4, 2, 1)
+        self.encoder2 = ResidualBlock(128, 128, time_emb_dim, text_emb_dim)
+        self.down2 = nn.Conv2d(128, 256, 4, 2, 1)
+        self.encoder3 = ResidualBlock(256, 256, time_emb_dim, text_emb_dim)
         
-        # Upsample
-        self.upstream = nn.ModuleList()
-        for i in range(len(up_channels)-1):
-            self.upstream.append(
-                Block(up_channels[i],up_channels[i+1], time_emb_dim, up=True)
-            )
-        
-        # Rescale to image dimension
-        self.output = nn.Conv2d(up_channels[-1], out_dim, 1)
+        self.middle = ResidualBlock(256, 256, time_emb_dim, text_emb_dim)
 
-    def forward(self, x, timestep):
-        # Embed time
-        t = self.time_mlp(timestep)
-        # Initial conv
-        x = self.conv0(x)
-        # Collect the individual outputs of the UNet and append them
-        # as residuals that are passed to the upstream decoder
-        residual_inputs = []
-        for down in self.downstream:
-            x = down(x, t)
-            residual_inputs.append(x)
-        for up in self.upstream:
-            residual_x = residual_inputs.pop()
-            # Add residual x as additional channels
-            x = torch.cat((x, residual_x), dim=1)           
-            x = up(x, t)
-        return self.output(x)
+        # Decoder-Part
+        self.up2 = nn.ConvTranspose2d(256, 128, 4, 2, 1)
+        self.decoder2 = ResidualBlock(256, 128, time_emb_dim, text_emb_dim)
+        self.up1 = nn.ConvTranspose2d(128, 64, 4, 2, 1)
+        self.decoder1 = ResidualBlock(128, 64, time_emb_dim, text_emb_dim)
+
+        self.final = nn.Conv2d(64, out_channels, 1)
+
+    def forward(self, x, t, text_raw):
+        t = self.time_mlp(t)
+        text = self.text_proj(text_raw)
+
+        x1 = self.encoder1(x, t, text)
+        x2 = self.encoder2(self.down1(x1), t, text)
+        x3 = self.encoder3(self.down2(x2), t, text)
+
+        x_mid = self.middle(x3, t, text)
+
+        x = self.decoder2(torch.cat([self.up2(x_mid), x2], dim=1), t, text)
+        x = self.decoder1(torch.cat([self.up1(x), x1], dim=1), t, text)
+
+        return self.final(x)
